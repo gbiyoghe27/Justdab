23f422c202f5e5c15f86c5deb48f9ee85aa66789
gbiyoghe27 <gcbiyoghe@gmail.com>
2024-07-26 16:06:01 +0200

    ajout fichier 4

--- src/test/java/com/training/justdab/JustdabApplicationTests.java
+++ src/test/java/com/training/justdab/JustdabApplicationTests.java
@@ -2,11 +2,30 @@ package com.training.justdab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class JustdabApplicationTests {
 
-	@Test
+	@Container
+	private static final MySQLContainer<?> SQL_CONTAINER = new MySQLContainer<>("mysql:latest")
+			.withDatabaseName("just_dab_db")
+			.withUsername("user")
+			.withPassword("password");
+
+	@DynamicPropertySource
+	static void configureMySqlProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", SQL_CONTAINER::getJdbcUrl);
+		registry.add("spring.datasource.username", SQL_CONTAINER::getUsername);
+		registry.add("spring.datasource.password", SQL_CONTAINER::getPassword);
+	}
+
+		@Test
 	void contextLoads() {
 	}
 
